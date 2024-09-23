992daa64f8f53d9670a8f732f8315007c96d2854
sumanthg <sumanth.g@khyath.com>
2024-09-23 14:02:33 +0530

    initial commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,19 +1,8 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello, World!";
+        System.out.println("Hello, World!");
     }
 }
+
